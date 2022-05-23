976cd6afe9abe5e04d10b03d4bcc1c9ae4fc37d8
Farabi9 <ifatkhan633@gmail.com>
2022-05-23 16:44:32 +0600

    my profile api

--- index.js
+++ index.js
@@ -1,34 +1,59 @@
 const express = require('express');
 const cors = require('cors');
-const jwt = require('jsonwebtoken');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 
 //middleware
-app.use(cors())
 app.use(express.json())
+app.use(cors())
+
+
+
 
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gkgv3.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-    try{
-           await client.connect();
-           const toolCollection = client.db('phone-pear').collection('tools');
+async function run() {
+    try {
+        await client.connect();
+        const toolCollection = client.db('phone-pear').collection('tools');
+        const userCollection = client.db('phone-pear').collection('user');
 
 
 
-           app.get('/tools', async(req, res) =>{
-               const query ={};
-               const result = await toolCollection.find(query).toArray();
-               res.send(result);
-           })
+        app.get('/tools', async (req, res) => {
+            const query = {};
+            const result = await toolCollection.find(query).toArray();
+            res.send(result);
+        })
+
+        app.get('/tools/:id', async (req, res) => {
+
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const tool = await toolCollection.findOne(query);
+            res.send(tool)
+        })
+        
+        app.put('/user/:email', async(req, res) =>{
+            const email = req.params.email;
+            const user = req.body.detail;
+            const filter = { email: email};
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await userCollection.updateOne(filter,updateDoc, options)
+            res.send(result)
+            
+
+        })
     }
-    finally{
+    finally {
 
     }
 }
