de48c8dcf86b7f349a6500833e8b151f1918e835
Farabi9 <ifatkhan633@gmail.com>
2022-05-26 14:55:19 +0600

    make admin

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId, ObjectID } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 //middleware
 app.use(express.json())
@@ -25,8 +26,33 @@ async function run() {
         const productCollection = client.db('phone-pear').collection('products');
         const reviewCollection = client.db('phone-pear').collection('reviews');
         const orderCollection = client.db('phone-pear').collection('orders');
+        const paymentCollection = client.db('phone-pear').collection('payments');
 
-
+        app.post('/create-payment-intent',  async (req, res) =>{
+            const product = req.body;
+            const price = product.prize;
+            const amount =  price * 100;
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: 'usd',
+                payment_method_types: ['card']
+            });
+            res.send( {clientSecret: paymentIntent.client_secret});
+        })
+        app.patch('/payment/:id',  async(req, res) => {
+            const id = req.params.id;
+            const payment= req.body;
+            const filter = {_id: ObjectId(id)};
+            const updatedDoc ={
+                $set:{
+               paid: true,
+               transactionId : payment.transactionId,
+                }
+            }
+            const result = await paymentCollection.insertOne(payment);
+            const updatedBooking = await paymentCollection.updateOne(filter, updatedDoc);
+            res.send(updatedBooking)
+        })
 
         app.get('/tools', async (req, res) => {
             const query = {};
@@ -94,6 +120,35 @@ async function run() {
         res.send(result)
     })
 
+    app.get('/payment/:id',  async(req, res) =>{
+        const id = req.params.id;
+        const query ={_id: ObjectId(id)};
+        const  user= await orderCollection.findOne(query);
+        res.send(user)
+    })
+    app.put('/user/admin/:email', async (req, res) => {
+        const email = req.params.email;
+        const filter = { email: email }
+        const updateDoc = {
+            $set: { role: 'admin' },
+        };
+        const result = await userCollection.updateOne(filter, updateDoc);
+        res.send(result)
+    })
+
+    app.get('/user/admin/:email', async(req, res) =>{
+        const email = req.params.email;
+        const query = {email: email}
+        const result = await userCollection.find(query).toArray()
+        res.send(result)
+
+    })
+    app.get('/user/admin/:email', async (req, res) => {
+        const email = req.params.email;
+        const user = await userCollection.findOne({ email: email })
+        const isAdmin = user.role === 'admin';
+        res.send({ admin: isAdmin })
+    })
 
     }
     finally {
